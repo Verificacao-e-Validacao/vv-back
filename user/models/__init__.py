52f310d55dab454296ccc2f7c7dc91e1d1ff3ed7
Dalton Campos <dalton.campos20@gmail.com>
2024-09-01 18:57:39 -0300

    feat: Criando model para gerente

---
+++ user/models/__init__.py
@@ -0,0 +1,5 @@
+from .gerente import Gerente
+
+__all__ = [
+    Gerente
+    ]
---
+++ user/models/gerente.py
@@ -0,0 +1,31 @@
+import uuid
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class Gerente(AbstractUser):
+
+    id = models.UUIDField(
+        primary_key=True, 
+        default=uuid.uuid4, 
+        editable=False
+    )
+
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='caixa_set',
+        blank=True,
+        help_text='The groups this user belongs to.',
+        verbose_name='groups',
+    )
+    
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='caixa_set',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions',
+    )
+
+    class Meta:
+        verbose_name = 'Gerente'
+        verbose_name_plural = 'Gerentes'
