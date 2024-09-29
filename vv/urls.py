e933d2e735779e3c2224a2a94e243c95f5718dfb
Dalton Campos <dalton.campos20@gmail.com>
2024-09-29 15:51:11 -0300

    feat: Adiciona rota de estoque

--- vv/urls.py
+++ vv/urls.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from user.viewsets import GerenteViewSet
-from produtos.viewsets import ProdutoViewSet
+from produtos.viewsets import ProdutoViewSet, EstoqueViewSet
+
 
 router = DefaultRouter()
 router.register(r'gerentes', GerenteViewSet)
 router.register(r'produtos', ProdutoViewSet)
+router.register(r'estoque', EstoqueViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
