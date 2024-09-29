968d13f8f4ae0f892aba622a5bb37937a26b786c
Dalton Campos <dalton.campos20@gmail.com>
2024-09-29 15:48:13 -0300

    feat: Adiciona estoque viewset

--- produtos/viewsets/__init__.py
+++ produtos/viewsets/__init__.py
@@ -1,5 +1,7 @@
 from .produto_viewset import ProdutoViewSet
+from .estoque_viewset import EstoqueViewSet
 
 __all__ = [
     ProdutoViewSet,
+    EstoqueViewSet,
 ]
---
+++ produtos/viewsets/estoque_viewset.py
@@ -0,0 +1,17 @@
+from rest_framework import filters, viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from ..models import Estoque
+from produtos.models import EstoqueSerializer
+
+
+class EstoqueViewSet(viewsets.ModelViewSet):
+    queryset = Estoque.objects.all()
+    serializer_class = EstoqueSerializer
+    permission_classes = [IsAuthenticated]
+
+    filter_backends = [filters.SearchFilter]
+
+    search_fields = [
+        
+    ]
