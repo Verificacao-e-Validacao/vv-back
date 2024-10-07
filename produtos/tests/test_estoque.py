cd4b06b01147cca2466bafe9c4f7ea92485e4562
Dalton Campos <dalton.campos20@gmail.com>
2024-10-06 21:31:08 -0300

    feat: Adiciona teste de update de estoque

--- produtos/tests/test_estoque.py
+++ produtos/tests/test_estoque.py
@@ -49,3 +49,19 @@ class EstoqueViewSetTest(APITestCase):
         response = self.client.post(self.url_list, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Estoque.objects.count(), 2)
+
+    def test_update_estoque(self):
+        """Testa se uma movimentação de estoque pode ser atualizada"""
+        url_detail = f'/api/estoque/{self.estoque.pk}/'
+        data = {
+            "produto": self.produto.pk,
+            "peso": "15.00",
+            "valor_compra": "60.00",
+            "quantidade": 80,
+            "vencimento": "2025-01-01"
+        }
+        response = self.client.put(url_detail, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.estoque.refresh_from_db()
+        self.assertEqual(self.estoque.peso, 15.00)
+        self.assertEqual(self.estoque.quantidade, 80)
