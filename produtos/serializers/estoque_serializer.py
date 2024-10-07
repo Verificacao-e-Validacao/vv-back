2bd0d032f0509f93c96d5012a032dc765d970dc3
Dalton Campos <dalton.campos20@gmail.com>
2024-10-06 21:41:08 -0300

    feat: Evita criação com quantidade negativa no serializer de estoque

--- produtos/serializers/estoque_serializer.py
+++ produtos/serializers/estoque_serializer.py
@@ -7,3 +7,8 @@ class EstoqueSerializer(serializers.ModelSerializer):
     class Meta:
         model = Estoque
         fields = '__all__'
+
+    def validate_quantidade(self, value):
+        if value < 0:
+            raise serializers.ValidationError("A quantidade não pode ser negativa.")
+        return value
