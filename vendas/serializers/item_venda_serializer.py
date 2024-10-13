1794aa438c431ca3e38c2ef68516d9a907869fbf
Dalton Campos <dalton.campos20@gmail.com>
2024-10-13 20:14:39 -0300

    feat: Adiciona serialzier de item venda

---
+++ vendas/serializers/item_venda_serializer.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from ..models import ItemVenda
+
+
+class ItemVendaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ItemVenda
+        fields = '__all__'
