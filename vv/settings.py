83e459a1110f8147961118cb218a6292505d5bcc
paulinaJulia <paulinajulia2015@gmail.com>
2024-10-21 23:37:20 -0300

    fix: Ajuste de segurança

--- vv/settings.py
+++ vv/settings.py
@@ -16,13 +16,9 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4o9w#9qzuw_49ver6o^#^b!yp*y)(ye(1!$l4(^&py!y=#eqjj'
-
+SECRET_KEY = config('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['mercado-facil.ufersa.dev.br','54.86.109.162', '127.0.0.1','localhost']
 CORS_ALLOW_ALL_ORIGINS = True
