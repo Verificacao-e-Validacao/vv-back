ab28c75448740bb64497e3e7c9bf544d2553804c
paulinaJulia <paulinajulia2015@gmail.com>
2024-10-21 23:53:10 -0300

    fix: Ajustes no settings

--- vv/settings.py
+++ vv/settings.py
@@ -162,7 +162,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATIC_ROOT = ('/var/www/html/static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
